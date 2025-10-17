b785d41f631e9cf2acc0d37683aef6a2cbacf3c2
Twahidul Islam <twahidulislam2005@gmail.com>
2025-10-18 01:30:26 +0600

    feat: refactor Project component and add ProjectCard for improved project display

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { FaCalendarDays, FaGithub } from 'react-icons/fa6'
+import { BsTag } from 'react-icons/bs'
+import { FiExternalLink } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
+const ProjectCard = ({ className, projectImage, projectTitle, projectDate, projectDescription, projectTechnologies, githubLink, liveLink }) => {
+    return (
+        <>
+            <div className={`w-full lg:w-[49%] bg-[#19151b] rounded-lg border border-gray-800 shadow shadow-gray-800 pb-20 relative ${className}`}>
+                {/* Project Image */}
+                <div className="w-full">
+                    <img className='w-full h-[200px] lg:h-[250px] rounded-lg object-cover' src={projectImage} alt={projectTitle} />
+                </div>
+                
+                {/* Project Content */}
+                <div className="mt-3 px-3" >
+                    {/* Project Title */}
+                    <h6 className='text-2xl font-medium'>{projectTitle}</h6>
+                    
+                    {/* Project Date */}
+                    <div className="flex justify-start items-center gap-x-3 mt-3">
+                        <FaCalendarDays className='text-neutral-400' />
+                        <p className='text-neutral-400'>{projectDate}</p>
+                    </div>
+                    
+                    {/* Project Description */}
+                    <p className='text-sm  text-neutral-400'>{projectDescription}</p>
+
+                    {/* Technologies part Start  */}
+                    <div className="flex justify-between items-center flex-wrap gap-3 pt-5">
+                        {/* Technology Tags */}
+                        {projectTechnologies?.map((tech, index) => (
+                            <div key={index} className="w-auto flex justify-between items-center gap-2 border border-white text-white px-2 lg:px-5 py-2 rounded-xl">
+                                <BsTag className='text-sm'/>
+                                <span>{tech}</span>
+                            </div>
+                        ))}
+                    </div>
+                    {/* Technologies part End  */}
+                </div>
+
+                {/* Github and Live Link part - Fixed at bottom */}
+                <div className="absolute bottom-0 left-0 right-0 flex items-center justify-between w-full max-w-xl mx-auto py-3 px-3 ">
+                    {/* GitHub Button  */}
+                    <Link to={githubLink}>
+                        <div className="flex items-center gap-2 bg-black text-white px-4 py-2 rounded-lg border border-gray-800 hover:bg-gray-900 transition hover:cursor-pointer">
+                            <span><FaGithub /></span>
+                            <span className="font-medium">Code</span>
+                        </div>
+                    </Link>
+
+                    {/* Live Demo Button */}
+                    <Link to={liveLink}>
+                        <div className="flex items-center gap-2 bg-black text-white px-4 py-2 rounded-lg border border-gray-800 hover:bg-gray-900 transition hover:cursor-pointer">
+                            <span><FiExternalLink /></span>
+                            <span className="font-medium">Live</span>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProjectCard
--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,154 +1,69 @@
 import React from 'react'
-import Container from '../Container'
 import ProjectOne from '/src/assets/projectOne.png'
 import ProjectTwo from '/src/assets/projectTwo.png'
-import { FaCalendarDays } from "react-icons/fa6";
-import { BsTag } from "react-icons/bs";
-import { FaGithub } from "react-icons/fa6";
-import { FiExternalLink } from "react-icons/fi";
-import { Link } from 'react-router-dom';
+import ProjectCard from '../ProjectCard';
+
 
 const Project = () => {
+  const projectsData = [
+    {
+      id: 1,
+      image: ProjectOne,
+      title: "Uomo E-Commerce Website",
+      date: "January 2024",
+      description: "A lightweight, production-ready microservice starterkit for Go applications with built-in middleware, observability, and clean architecture patterns.",
+      technologies: ["Figma", "Tailwind", "React"],
+      githubLink: "https://github.com/twahidulislamdev/uomo",
+      liveLink: "https://uomo-black.vercel.app/",
+      className: "w-full lg:w-[49%]",
+      imageHeight: "h-[200px] lg:h-[250px]"
+    },
+    {
+      id: 2,
+      image: ProjectTwo,
+      title: "Creative Agency Website",
+      date: "December 2023",
+      description: "A modern creative agency website with responsive design and smooth animations, observability, and clean architecture patterns. ",
+      technologies: ["Figma", "Tailwind", "React"],
+      githubLink: "https://github.com/twahidulislamdev/creative-agency",
+      liveLink: "https://twahidulislamdev.github.io/creative-agency/",
+      className: "w-full lg:w-[49%]",
+      imageHeight: "h-[200px] lg:h-[250px]"
+    }
+  ];
+
   return (
-    <>
-      <div className="mt-5 px-2 ">
-          <div className="flex justify-start items-center gap-x-2">
-            <div className="w-4.5 h-7.5 bg-amber-400 rounded"></div>
-            <h3 className='text-3xl font-semibold'>Portfolio</h3>
-          </div>
-          <ul className='flex justify-start items-center gap-x-10 mt-8'>
-            <li className='text-base font-semibold hover:text-yellow-400'>All</li>
-            <li className='text-base font-semibold hover:text-yellow-400'>Application</li>
-            <li className='text-base font-semibold hover:text-yellow-400'>Web Development</li>
-            <li className='text-base font-semibold hover:text-yellow-400'>Mobile Apps</li>
-          </ul>
-          <div className="w-full mt-8 flex justify-between gap-x-3 gap-y-10 flex-wrap">
-            {/* First project part start  */}
-            <div className="w-[90%] lg:w-[49%] h-[750px] lg:h-[550px] m-auto border-1 border-gray rounded-lg">
-              <div className="w-full">
-                <img className='w-full h-[500px] lg:h-[240px] rounded-lg ' src={ProjectOne} alt="" />
-              </div>
-              <div className="mt-3 px-3 " >
-                <h6 className='text-2xl font-medium '>Uomo E-Commarce web Site</h6>
-                <div className="flex justify-start items-center gap-x-3 mt-3">
-                  <FaCalendarDays className='text-neutral-400' />
-                  <p className='text-neutral-400'>Date</p>
-                </div>
-                <p className='text-sm mt-3 text-neutral-400'>A lightweight, production-ready microservice starterkit for Go applications with built-in middleware, observability, and clean architecture patterns.</p>
-
-                {/* Futures and Technology part Start  */}
-                <div className="grid grid-cols-3 gap-3  pt-5">
-                  {/* Tag */}
-                  <div className="flex items-center gap-2 border border-white text-white px-4 py-2 rounded-xl">
-                    <BsTag />
-                    <span>Figma</span>
-                  </div>
-                  {/* Tag */}
-                  <div className="flex items-center gap-2 border border-white text-white px-4 py-2 rounded-xl">
-                    <BsTag />
-                    <span>Tailwind</span>
-                  </div>
-                  {/* Tag */}
-                  <div className="flex items-center gap-2 border border-white text-white px-4 py-2 rounded-xl">
-                    <BsTag />
-                    <span>React</span>
-                  </div>
-                </div>
-                {/* Futures and Technology part End  */}
-
-                {/* Github and Live Link part Start  */}
-                <div class="flex items-center justify-between w-full max-w-xl mx-auto py-6">
-                  {/* Left Button  */}
-                  <Link to={"https://github.com/twahidulislamdev/uomo"}>
-                    <div
-                      class="flex items-center gap-2 bg-black text-white px-4 py-2 rounded-lg border border-gray-800 hover:bg-gray-900 transition hover:cursor-pointer"
-                    >
-                      <span><FaGithub /></span>
-                      <span class="font-medium">Code</span>
-                    </div>
-                  </Link>
-
-
-                  {/* Right Button */}
-                  <Link to={"https://uomo-black.vercel.app/"}>
-                    <div
-                      class="flex items-center gap-2 bg-black text-white px-4 py-2 rounded-lg border border-gray-800 hover:bg-gray-900 transition hover:cursor-pointer"
-                    >
-                      <span><FiExternalLink /></span>
-                      <span class="font-medium">Live </span>
-                    </div>
-                  </Link>
-                </div>
-                {/* Github and Live Link part End  */}
-              </div>
-            </div>
-            {/* First project part End  */}
-            {/* Second project part start  */}
-            <div className="w-[90%] lg:w-[49%] h-[750px] lg:h-[550px] m-auto border-1 border-gray rounded-lg">
-              <div className="w-full">
-                <img className='w-full h-[500px] lg:h-[240px] rounded-lg' src={ProjectTwo} alt="" />
-              </div>
-              <div className="mt-3 px-3 " >
-                <h6 className='text-2xl font-medium '>Uomo E-Commarce web Site</h6>
-                <div className="flex justify-start items-center gap-x-3 mt-3">
-                  <FaCalendarDays className='text-neutral-400' />
-                  <p className='text-neutral-400'>Date</p>
-                </div>
-                <p className='text-sm mt-3 text-neutral-400'>A lightweight, production-ready microservice starterkit for Go applications with built-in middleware, observability, and clean architecture patterns.</p>
-
-                {/* Futures and Technology part Start  */}
-                <div className="grid grid-cols-3 gap-3  pt-5">
-                  {/* Tag */}
-                  <div className="flex items-center gap-2 border border-white text-white px-4 py-2 rounded-xl">
-                    <BsTag />
-                    <span>Figma</span>
-                  </div>
-                  {/* Tag */}
-                  <div className="flex items-center gap-2 border border-white text-white px-4 py-2 rounded-xl">
-                    <BsTag />
-                    <span>Tailwind</span>
-                  </div>
-                  {/* Tag */}
-                  <div className="flex items-center gap-2 border border-white text-white px-4 py-2 rounded-xl">
-                    <BsTag />
-                    <span>React</span>
-                  </div>
-                </div>
-                {/* Futures and Technology part End  */}
-
-                {/* Github and Live Link part Start  */}
-                <div class="flex items-center justify-between w-full max-w-xl mx-auto py-6">
-                  {/* Left Button  */}
-                  <Link to={"https://github.com/twahidulislamdev/creative-agency"}>
-                    <div
-                      href="#"
-                      class="flex items-center gap-2 bg-black text-white px-4 py-2 rounded-lg border border-gray-800 hover:bg-gray-900 transition hover:cursor-pointer"
-                    >
-                      <span><FaGithub /></span>
-                      <span class="font-medium">Code</span>
-                    </div>
-                  </Link>
-
-                  {/* Right Button */}
-                  <Link to={"https://twahidulislamdev.github.io/creative-agency/"}>
-                    <div
-                      href="#"
-                      class="flex items-center gap-2 bg-black text-white px-4 py-2 rounded-lg border border-gray-800 hover:bg-gray-900 transition hover:cursor-pointer"
-                    >
-                      <span><FiExternalLink /></span>
-                      <span class="font-medium">Link</span>
-                    </div>
-                  </Link>
-
-                </div>
-                {/* Github and Live Link part End  */}
-              </div>
-            </div>
-            {/* Second project part End  */}
-          </div>
-
+    <div className="mt-5 mb-20 lg:mb-0">
+      <div className="flex justify-start items-center gap-x-2">
+        <div className="w-4.5 h-7.5 bg-amber-400 rounded"></div>
+        <h3 className='text-3xl font-semibold'>Portfolio</h3>
       </div>
-    </>
+
+      <ul className='flex justify-start items-center gap-x-10 mt-8'>
+        <li className='text-base font-semibold hover:text-yellow-400'>All</li>
+        <li className='text-base font-semibold hover:text-yellow-400'>Application</li>
+        <li className='text-base font-semibold hover:text-yellow-400'>Web Development</li>
+        <li className='text-base font-semibold hover:text-yellow-400'>Mobile Apps</li>
+      </ul>
+
+      {/* Projects part */}
+      <div className="w-full mt-8 flex justify-between gap-x-3 gap-y-10 flex-wrap">
+        {projectsData.map(project => (
+          <ProjectCard
+            key={project.id}
+            className={project.className}
+            projectImage={project.image}
+            projectTitle={project.title}
+            projectDate={project.date}
+            projectDescription={project.description}
+            projectTechnologies={project.technologies}
+            githubLink={project.githubLink}
+            liveLink={project.liveLink}
+            imageHeight={project.imageHeight}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
